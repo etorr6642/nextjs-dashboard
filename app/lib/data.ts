204aeb517a4ee2060153f983c8d5d4c7519e545b
Edward Torres <edtorres@csumb.edu>
2025-11-01 13:55:52 -0700

    Chapter 8 Completed

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -36,7 +36,7 @@ export async function fetchLatestInvoices() {
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices
       JOIN customers ON invoices.customer_id = customers.id
-      ORDER BY invoices.date DESC
+      ORDER BY invoices.date DESC, invoices.id DESC
       LIMIT 5`;
 
     const latestInvoices = data.map((invoice) => ({
