af00b8623b58f843584578a25ed55431a40a2350
Edward Torres <edtorres@csumb.edu>
2025-11-14 11:22:56 -0800

    Completed Ch 10/11

---
+++ app/dashboard/invoices/create/page.tsx
@@ -0,0 +1,25 @@
+import Form from '@/app/ui/invoices/create-form';
+import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
+import { fetchCustomers } from '@/app/lib/data';
+ 
+export default async function Page() {
+    //calls customers from data.ts
+  const customers = await fetchCustomers();
+ 
+  return (
+    <main>
+        {/* calls breadcrumbs from ui/invoices/breadcrumbs */}
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: 'Invoices', href: '/dashboard/invoices' },
+          {
+            label: 'Create Invoice',
+            href: '/dashboard/invoices/create',
+            active: true,
+          },
+        ]}
+      />
+      <Form customers={customers} />
+    </main>
+  );
+}
